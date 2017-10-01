165011442a95acee293c7fe4f75947199166d4cc
Denes Toth <denes.toth@outlook.com>
2017-10-01 16:32:44 +0200

    it is alive

--- src/main/java/com/epam/spring/advanced/homework/controllers/MainController.java
+++ src/main/java/com/epam/spring/advanced/homework/controllers/MainController.java
@@ -2,16 +2,19 @@ package com.epam.spring.advanced.homework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author Denes Toth
  */
 @Controller
-public class SomeController {
+@RequestMapping("/")
+public class MainController {
 
+    @RequestMapping(method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("someAttribute","someValue");
         return "index";
     }
-
 }
